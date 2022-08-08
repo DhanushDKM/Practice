45178c0900afefdfd79bd7bd2c968d78f7412835
DhanushDKM <110026584+DhanushDKM@users.noreply.github.com>
2022-08-08 11:39:07 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,24 @@
+import java.util.*
+class Test
+{
+	public static void main(String[] Sname)
+    {
+		
+        int valuel=10; 
+        int value2=20;
+
+        System.out.println(valuel);
+
+        System.out.println(value2);
+
+        System.out.println("First value"+ valuel); 
+
+        System.out.println("Second value"+ value2);
+
+        System.out.println(valuel + value +" Values are ");
+
+        System.out.println("Values are" valuel * value2); 
+
+        System.out.println("Values are" +(valuel + value2));
+	}
+}	
---
+++ submark.java
@@ -0,0 +1,24 @@
+class submark
+{
+    public static void main(String[]ref)
+    {
+    int id = 1234;
+    String name= "Steve";
+    String subject= "Maths";
+    int mark = 35;
+    char initial = 'R'; 
+    boolean information =true;
+
+    System.out.println(id); I
+
+    System.out.println(name); 
+
+    System.out.println(subject);
+
+    System.out.println(mark);
+
+    System.out.println(initial);
+
+    System.out.println(information);
+    }
+}
